e68f0c0d96b390f84f25185aa8a17402120fa68c
root <root@DESKTOP-QI1L4PT.localdomain>
2019-03-29 16:51:29 +0430

    utility added

---
+++ utility.py
@@ -0,0 +1,27 @@
+def purify(line):
+	for i in line:
+		if len(i) == 0:
+			line.remove(i)
+
+def parser(line):
+		ports = []
+		names = line.split('(')[0].split(' ')
+		
+		t = names[0]
+		type = ""
+		if "XNOR" in t or "XOR" in t:
+			type = "XOR"
+		elif "AND" in t:
+			type = "AND"
+		elif "OR" in t:
+			type = "OR"
+
+		purify(names)
+
+		line = line.split('.')[1:]
+		for i in line:
+			s = i.split('(')[1].split(')')
+			# purify(s)
+			ports.append(s[0])
+
+		return type, names[1], ports[0: -1], ports[-1]
