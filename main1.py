caaae25b914b36b7e19a2714b776f8727595e0fe
miladHakimi <miladhakimi33@gmail.com>
2019-03-29 23:08:47 +0430

    done :)

--- main1.py
+++ main1.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 from utility import *
 from Node import Node
 from Module import Module
@@ -10,11 +12,11 @@ if __name__ == '__main__':
 	f = open("part1.v", "r")
 	
 	for i in f:
+		input_nodes = []
 		lower_case = i.lower()
 		if "inv" in lower_case or "and" in lower_case or "or" in lower_case:
-			type, name, inputs, output = parser(i)
+			t, name, inputs, output = parser(i)
 			for i in inputs:
-				input_nodes = []
 				if i not in nodes:
 					nodes[i] = Node(i)	
 
@@ -22,9 +24,32 @@ if __name__ == '__main__':
 				nodes[output] = output_node
 				input_nodes.append(nodes[i])
 
-			modules.append(Module(type, name, input_nodes, output_node))
+			modules.append(Module(t, name, input_nodes, output_node))
 
 	f = open("input.txt", "r")
 
+	A = {}
+	B = {}
+	count = 0
+
 	for i in f:
-		
+		count += 1
+		for j in range(8):
+			index = str(j)
+			if index not in A:
+				A[index] = 0
+				B[index] = 0
+
+			A[index] += int(i[j])
+			B[index] += int(i[j+8])
+
+	for i in A:
+		nodes["A["+i+"]"].p = A[i]/count
+	for i in B:
+		nodes["B["+i+"]"].p = B[i]/count
+
+	for i in modules:
+		i.calc()
+
+	for i in nodes:
+		print(nodes[i])
