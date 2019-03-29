9e9c7cc71910f25d9be45deb7b259b0025e29524
miladHakimi <miladhakimi33@gmail.com>
2019-03-29 23:07:31 +0430

    str method added.

--- Node.py
+++ Node.py
@@ -4,3 +4,9 @@ class Node:
 	def __init__(self, name):
 		self.name = name
 		self.p = 0
+
+	def get_activity():
+		return self.p*(1-self.p)
+	
+	def __str__(self):
+		return self.name + '{\n' + '	P(n=1) = ' + str(self.p) + '\n	P(n=0) = ' + str(1-self.p) + '\n	P(0->1) = ' + str(self.p*(1-self.p)) + '\n}'
