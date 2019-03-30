1f0ca46ea7eb68e95a248a2353aa0bd838e911ef
miladHakimi <miladhakimi33@gmail.com>
2019-03-30 12:45:14 +0430

    some minor improvements :)

--- Module.py
+++ Module.py
@@ -2,10 +2,12 @@ from utility import *
 
 class Module:
 	def __init__(self, type, name, input_nodes, output_node):
+		
 		self.name = name
 		self.type = type
 		self.input_nodes = input_nodes
 		self.output_node = output_node
+		self.leakage = 0
 
 		self.funcs = {
 			"INV": self.INV,
@@ -16,6 +18,15 @@ class Module:
 			"NAND": self.NANAD,
 			"NOR": self.NOR
 		}
+
+		self.leakage_funcs = {
+			"NAND": self.leakage_nand,
+			"INV": self.leakage_inv,
+			"NOR": self.leakage_nor
+		}
+
+		self.done = False
+
 	def INV(self):
 		return 1-self.input_nodes[0].p
 
@@ -48,3 +59,45 @@ class Module:
 
 	def calc(self):
 		self.output_node.p = self.funcs[self.type]()
+
+	def leakage_nand(self):
+		if int(self.input_nodes[0].value) ^ int(self.input_nodes[1].value) == 1:
+			data = "01"
+		else:
+			data = self.input_nodes[0].value*2
+
+		nanadMap = {
+			"00": 11,
+			"01": 38,
+			"11": 58
+		}
+		return nanadMap[data], str(int(not (self.input_nodes[0].value and self.input_nodes[1].value)))
+	def leakage_inv(self):
+		data = self.input_nodes[0].value
+		invMap = {
+			"0": 19,
+			"1": 18
+		}
+
+		return invMap[data], str(int(not self.input_nodes[0].value))
+	def leakage_nor(self):
+		if int(self.input_nodes[0].value) ^ int(self.input_nodes[1].value) == 1:
+			data = "01"
+		else:
+			data = self.input_nodes[0].value*2
+
+		norMap = {
+			"00": 47,
+			"01": 30,
+			"11": 25
+		}
+		return norMap[data], str(int(not (self.input_nodes[0].value or self.input_nodes[1].value)))
+
+	def calc_leakage(self):
+		
+		for i in self.input_nodes:
+			if i.value == -1:
+				return False		
+		self.leakage, self.output_node.value = self.leakage_funcs[self.type]()
+		self.done = True
+		return True
