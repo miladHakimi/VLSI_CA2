ce65d3846db978a24437ed5edfc5f965ad45aff1
miladHakimi <miladhakimi33@gmail.com>
2019-03-30 11:33:45 +0430

    some bugs are fixed!

--- main1.py
+++ main1.py
@@ -16,14 +16,17 @@ if __name__ == '__main__':
 		lower_case = i.lower()
 		if "inv" in lower_case or "and" in lower_case or "or" in lower_case:
 			t, name, inputs, output = parser(i)
+			if output not in nodes:
+				nodes[output] = Node(output)
+
 			for i in inputs:
 				if i not in nodes:
 					nodes[i] = Node(i)	
 
-				output_node = Node(output)
 				nodes[output] = output_node
 				input_nodes.append(nodes[i])
-
+			
+			output_node = nodes[output]
 			modules.append(Module(t, name, input_nodes, output_node))
 
 	f = open("input.txt", "r")
